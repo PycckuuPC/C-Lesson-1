13e2cfade9316f225d416ecded848ee93633a083
Nikolay Yakushenko <pycckuupc@gmail.com>
2022-09-24 12:36:28 +0300

    all seminar tasks

---
+++ task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два целых числа");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+if(A > B) /*A - наибольшее*/
+    Console.WriteLine(A + " большее число");
+    else /*B - наибольшее*/
+    Console.WriteLine(B + " большее число");
---
+++ task 4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите три целых числа");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(A > B)
+    if(A > C)   /*A - наибольшее*/
+    Console.WriteLine(A + " большее число");
+    else /*С - наибольшее*/
+    Console.WriteLine(C + " большее число");
+else
+    if(B > C)   /*B - наибольшее*/
+    Console.WriteLine(B + " большее число");
+    else /*С - наибольшее*/
+    Console.WriteLine(C + " большее число");
